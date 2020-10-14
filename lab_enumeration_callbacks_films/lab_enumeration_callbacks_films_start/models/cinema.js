906e3874d9b5b6e3924be174a83cab2d5d28b66b
svvishjs <andreww92@live.com>
2020-10-14 14:40:52 +0100

    added getFilmTitles function which passes

--- lab_enumeration_callbacks_films/lab_enumeration_callbacks_films_start/models/cinema.js
+++ lab_enumeration_callbacks_films/lab_enumeration_callbacks_films_start/models/cinema.js
@@ -1,5 +1,13 @@
 const Cinema = function (films) {
-  this.films = films;
+	this.films = films;
+};
+
+Cinema.prototype.getFilmTitles = function () {
+	const filmTitles = this.films.map((film) => {
+		return film.title;
+	});
+
+	return filmTitles;
 };
 
 module.exports = Cinema;
